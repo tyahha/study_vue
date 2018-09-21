773334c36f9e0e5f5075e29e4c52622c555360bc
佐藤利徳 <tyahhahha@gmail.com>
2018-09-22 01:21:01 +0900

    study event

--- event/main.js
+++ event/main.js
@@ -1,8 +1,14 @@
 var app = new Vue({
   el: '#app',
+  data: {
+    count: 0,
+  },
   methods: {
     handleClick: function(event) {
       alert(event.target);
+    },
+    log(text) {
+      console.log(text);
     }
   }
 });
