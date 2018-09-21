d74c64422a532efa7d613caf8571e28f36f241c2
tyahha <tyahhahha@gmail.com>
2018-09-21 23:19:51 +0900

    study component(途中)

---
+++ component/main.js
@@ -0,0 +1,55 @@
+Vue.component('comp-child', {
+  template: '<p>{{ val }}</p>',
+  props: ['val'],
+})
+
+Vue.component('my-component', {
+  template: '<p>\
+               <comp-child v-bind:val="val"></comp-child>\
+               <comp-child val="これは子B"></comp-child>\
+             </p>',
+  props: ['val'],
+  data: function() {
+    return {
+      message: 'Hello Vue.js'
+    }
+  },
+  methods: {
+  }
+})
+
+Vue.component('comp-child2', {
+  template: '<li>{{ name }} HP.{{ hp }}\
+              <button v-on:click="doAttack">攻撃する</button></li>',
+  props: {
+    id: Number,
+    name: String,
+    hp: Number,
+  },
+  methods: {
+    doAttack: function() {
+      this.$emit('attack', this.id)
+    }
+  }
+})
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'abc',
+    list: [
+      { id: 1, name: 'スライム', hp: 100 },
+      { id: 2, name: 'ゴブリん', hp: 200 },
+      { id: 3, name: 'ドラゴン', hp: 500 },
+    ]
+  },
+  methods: {
+    handleAttack: function(id) {
+      var item = this.list.find(function(el) {
+        return el.id === id
+      });
+      if (item && item.hp > 0) item.hp -= 10;
+    }
+  }
+});
+
